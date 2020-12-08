cc083593b12e0592ca54509fc652189e5659d4f8
Walid MELLOULI <walid_mellouli@carrefour.com>
2020-12-08 11:21:52 +0100

    Use context WithTimeout for elasticsearch main functions. close #4

--- pkg/utils/elasticsearch.go
+++ pkg/utils/elasticsearch.go
@@ -21,6 +21,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
@@ -28,11 +29,12 @@ import (
 )
 
 const (
-	StatusCreated = "Created"
-	StatusRetry   = "Retry"
-	StatusError   = "Error"
-	Index         = "Index"
-	Template      = "Template"
+	StatusCreated                      = "Created"
+	StatusRetry                        = "Retry"
+	StatusError                        = "Error"
+	Index                              = "Index"
+	Template                           = "Template"
+	ElasticMainFnTimeout time.Duration = 10 * time.Second
 )
 
 type EsConfig struct {
@@ -105,8 +107,11 @@ func (es Elasticsearch) NewClient(config *EsConfig, log logr.Logger) (*Elasticse
 	return &es, nil
 }
 
-func (es *Elasticsearch) PingES() error {
-	info, err := es.Client.Info()
+func (es *Elasticsearch) PingES(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, ElasticMainFnTimeout)
+	defer cancel()
+
+	info, err := esapi.InfoRequest{}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while connecting to elasticsearch", "host", es.Config)
 		return err
@@ -116,8 +121,8 @@ func (es *Elasticsearch) PingES() error {
 	return nil
 }
 
-func (es *Elasticsearch) existsIndex(indexName string) *bool {
-	response, err := es.Client.Indices.Exists([]string{indexName})
+func (es *Elasticsearch) existsIndex(ctx context.Context, indexName string) *bool {
+	response, err := esapi.IndicesExistsRequest{Index: []string{indexName}}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while checking index exists", "indexName", indexName)
 		return nil
@@ -127,8 +132,8 @@ func (es *Elasticsearch) existsIndex(indexName string) *bool {
 	return &is2xxStatusCode
 }
 
-func (es *Elasticsearch) existsTemplate(templateName string) *bool {
-	response, err := es.Client.Indices.ExistsTemplate([]string{templateName})
+func (es *Elasticsearch) existsTemplate(ctx context.Context, templateName string) *bool {
+	response, err := esapi.IndicesExistsTemplateRequest{Name: []string{templateName}}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while checking template exists", "templateName", templateName)
 		return nil
@@ -138,8 +143,8 @@ func (es *Elasticsearch) existsTemplate(templateName string) *bool {
 	return &is2xxStatusCode
 }
 
-func (es *Elasticsearch) getNumberOfReplicasAndShards(indexName string) (*int32, *int32) {
-	response, err := esapi.IndicesGetSettingsRequest{Index: []string{indexName}}.Do(context.Background(), es.Client)
+func (es *Elasticsearch) getNumberOfReplicasAndShards(ctx context.Context, indexName string) (*int32, *int32) {
+	response, err := esapi.IndicesGetSettingsRequest{Index: []string{indexName}}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while getting settings", "indexName", indexName)
 		return nil, nil
@@ -155,9 +160,9 @@ func (es *Elasticsearch) getNumberOfReplicasAndShards(indexName string) (*int32,
 	return replicas, shards
 }
 
-func (es *Elasticsearch) getProperties(indexName string) (*string, error) {
+func (es *Elasticsearch) getProperties(ctx context.Context, indexName string) (*string, error) {
 	no := false
-	response, err := esapi.IndicesGetMappingRequest{Index: []string{indexName}, IncludeTypeName: &no}.Do(context.Background(), es.Client)
+	response, err := esapi.IndicesGetMappingRequest{Index: []string{indexName}, IncludeTypeName: &no}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while getting properties", "indexName", indexName)
 		return nil, err
@@ -172,9 +177,9 @@ func (es *Elasticsearch) getProperties(indexName string) (*string, error) {
 	return (&EsMappings{Mappings: mappings}).GetProperties(indexName), nil
 }
 
-func (es *Elasticsearch) updateIndexReplicas(indexName string, numReplicas int32) (int, string, error) {
+func (es *Elasticsearch) updateIndexReplicas(ctx context.Context, indexName string, numReplicas int32) (int, string, error) {
 	settings := strings.NewReader(fmt.Sprintf(`{"index" : {"number_of_replicas" : %v}}`, numReplicas))
-	response, err := esapi.IndicesPutSettingsRequest{Index: []string{indexName}, Body: settings}.Do(context.Background(), es.Client)
+	response, err := esapi.IndicesPutSettingsRequest{Index: []string{indexName}, Body: settings}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while updating number_of_replicas", "indexName", indexName, "replicas", numReplicas)
 		return -1, "", err
@@ -185,10 +190,10 @@ func (es *Elasticsearch) updateIndexReplicas(indexName string, numReplicas int32
 	return statusCode, responseStr, nil
 }
 
-func (es *Elasticsearch) updateIndexMapping(indexName string, properties string) (int, string, error) {
+func (es *Elasticsearch) updateIndexMapping(ctx context.Context, indexName string, properties string) (int, string, error) {
 	mappingReader := strings.NewReader(properties)
 	no := false
-	response, err := esapi.IndicesPutMappingRequest{Index: []string{indexName}, Body: mappingReader, IncludeTypeName: &no}.Do(context.Background(), es.Client)
+	response, err := esapi.IndicesPutMappingRequest{Index: []string{indexName}, Body: mappingReader, IncludeTypeName: &no}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while updating mapping", "indexName", indexName, "mapping", properties)
 		return -1, "", err
@@ -199,9 +204,11 @@ func (es *Elasticsearch) updateIndexMapping(indexName string, properties string)
 	return statusCode, responseStr, nil
 }
 
-func (es *Elasticsearch) CreateOrUpdateIndex(indexName string, model string) (*EsStatus, error) {
-	exists := es.existsIndex(indexName)
+func (es *Elasticsearch) CreateOrUpdateIndex(ctx context.Context, indexName string, model string) (*EsStatus, error) {
+	ctx, cancel := context.WithTimeout(ctx, ElasticMainFnTimeout)
+	defer cancel()
 
+	exists := es.existsIndex(ctx, indexName)
 	if exists == nil {
 		errMsg := "error while checking index exists"
 		es.log.Error(nil, errMsg, "indexName", indexName)
@@ -209,11 +216,11 @@ func (es *Elasticsearch) CreateOrUpdateIndex(indexName string, model string) (*E
 	} else if *exists {
 		es.log.Info("index already exists", "indexName", indexName)
 
-		if status, err := es.updateIndexSettings(indexName, model); status != nil || err != nil {
+		if status, err := es.updateIndexSettings(ctx, indexName, model); status != nil || err != nil {
 			return status, err
 		}
 
-		if status, err := es.updateIndexProperties(indexName, model); status != nil || err != nil {
+		if status, err := es.updateIndexProperties(ctx, indexName, model); status != nil || err != nil {
 			return status, err
 		}
 
@@ -221,7 +228,7 @@ func (es *Elasticsearch) CreateOrUpdateIndex(indexName string, model string) (*E
 	}
 
 	shouldIncludeTypeName := (&EsModel{Model: model}).IsMappingWithType()
-	response, err := esapi.IndicesCreateRequest{Index: indexName, Body: strings.NewReader(model), IncludeTypeName: shouldIncludeTypeName}.Do(context.Background(), es.Client)
+	response, err := esapi.IndicesCreateRequest{Index: indexName, Body: strings.NewReader(model), IncludeTypeName: shouldIncludeTypeName}.Do(ctx, es.Client)
 
 	if err != nil {
 		es.log.Error(err, "error while creating index", "indexName", indexName)
@@ -240,8 +247,8 @@ func (es *Elasticsearch) CreateOrUpdateIndex(indexName string, model string) (*E
 	return BuildEsStatus(response.StatusCode, response.String()), nil
 }
 
-func (es *Elasticsearch) updateIndexSettings(indexName string, model string) (*EsStatus, error) {
-	oldNumReplicas, oldNumShards := es.getNumberOfReplicasAndShards(indexName)
+func (es *Elasticsearch) updateIndexSettings(ctx context.Context, indexName string, model string) (*EsStatus, error) {
+	oldNumReplicas, oldNumShards := es.getNumberOfReplicasAndShards(ctx, indexName)
 	numReplicas, err := (&EsModel{Model: model}).GetNumberOfReplicas()
 	numShards, err2 := (&EsModel{Model: model}).GetNumberOfShards()
 
@@ -267,7 +274,7 @@ func (es *Elasticsearch) updateIndexSettings(indexName string, model string) (*E
 	isReplicasUpdated := (oldNumReplicas == nil || *oldNumReplicas != *numReplicas) && err == nil && err2 == nil
 	if isReplicasUpdated {
 		es.log.Info("index already exists and updating number_of_replicas", "indexName", indexName, "from", *oldNumReplicas, "to", *numReplicas)
-		statusCode, responseStr, err := es.updateIndexReplicas(indexName, *numReplicas)
+		statusCode, responseStr, err := es.updateIndexReplicas(ctx, indexName, *numReplicas)
 		if err != nil {
 			errMsg := fmt.Sprintf("error while updating number_of_replicas from %v to %v", *oldNumReplicas, *numReplicas)
 			es.log.Error(err, errMsg, "indexName", indexName)
@@ -285,8 +292,8 @@ func (es *Elasticsearch) updateIndexSettings(indexName string, model string) (*E
 	return nil, nil
 }
 
-func (es *Elasticsearch) updateIndexProperties(indexName string, model string) (*EsStatus, error) {
-	oldProperties, err := es.getProperties(indexName)
+func (es *Elasticsearch) updateIndexProperties(ctx context.Context, indexName string, model string) (*EsStatus, error) {
+	oldProperties, err := es.getProperties(ctx, indexName)
 	properties := (&EsModel{Model: model}).GetProperties()
 
 	if err != nil {
@@ -306,7 +313,7 @@ func (es *Elasticsearch) updateIndexProperties(indexName string, model string) (
 			return &EsStatus{Status: StatusError, Message: errMsg}, errors.New(errMsg)
 		}
 
-		statusCode, responseStr, err := es.updateIndexMapping(indexName, *properties)
+		statusCode, responseStr, err := es.updateIndexMapping(ctx, indexName, *properties)
 		if err != nil {
 			errMsg := fmt.Sprintf("error while updating properties from %v to %v", *oldProperties, *properties)
 			es.log.Error(err, errMsg, "indexName", indexName)
@@ -324,14 +331,17 @@ func (es *Elasticsearch) updateIndexProperties(indexName string, model string) (
 	return nil, nil
 }
 
-func (es *Elasticsearch) DeleteIndex(indexName string) error {
-	exists := es.existsIndex(indexName)
+func (es *Elasticsearch) DeleteIndex(ctx context.Context, indexName string) error {
+	ctx, cancel := context.WithTimeout(ctx, ElasticMainFnTimeout)
+	defer cancel()
+
+	exists := es.existsIndex(ctx, indexName)
 	if exists != nil && !*exists {
 		es.log.Info("index cannot be deleted because it does not exists", "indexName", indexName)
 		return nil
 	}
 
-	response, err := es.Client.Indices.Delete([]string{indexName})
+	response, err := esapi.IndicesDeleteRequest{Index: []string{indexName}}.Do(ctx, es.Client)
 	if err != nil {
 		es.log.Error(err, "error while deleting index", "indexName", indexName)
 		return err
@@ -348,11 +358,14 @@ func (es *Elasticsearch) DeleteIndex(indexName string) error {
 	return nil
 }
 
-func (es *Elasticsearch) CreateOrUpdateTemplate(templateName string, model string) (*EsStatus, error) {
-	exists := es.existsTemplate(templateName)
+func (es *Elasticsearch) CreateOrUpdateTemplate(ctx context.Context, templateName string, model string) (*EsStatus, error) {
+	ctx, cancel := context.WithTimeout(ctx, ElasticMainFnTimeout)
+	defer cancel()
+
+	exists := es.existsTemplate(ctx, templateName)
 
 	shouldIncludeTypeName := (&EsModel{Model: model}).IsMappingWithType()
-	response, err := esapi.IndicesPutTemplateRequest{Name: templateName, Body: strings.NewReader(model), IncludeTypeName: shouldIncludeTypeName}.Do(context.Background(), es.Client)
+	response, err := esapi.IndicesPutTemplateRequest{Name: templateName, Body: strings.NewReader(model), IncludeTypeName: shouldIncludeTypeName}.Do(ctx, es.Client)
 	if err != nil || exists == nil {
 		es.log.Error(err, "error while creating template", "templateName", templateName)
 		return &EsStatus{Status: StatusError, Message: err.Error()}, err
@@ -375,14 +388,17 @@ func (es *Elasticsearch) CreateOrUpdateTemplate(templateName string, model strin
 	return BuildEsStatus(response.StatusCode, response.String()), nil
 }
 
-func (es *Elasticsearch) DeleteTemplate(templateName string) error {
-	exists := es.existsTemplate(templateName)
+func (es *Elasticsearch) DeleteTemplate(ctx context.Context, templateName string) error {
+	ctx, cancel := context.WithTimeout(ctx, ElasticMainFnTimeout)
+	defer cancel()
+
+	exists := es.existsTemplate(ctx, templateName)
 	if exists != nil && !*exists {
 		es.log.Info("template cannot be deleted because it does not exists", "templateName", templateName)
 		return nil
 	}
 
-	response, err := es.Client.Indices.DeleteTemplate(templateName)
+	response, err := esapi.IndicesDeleteTemplateRequest{Name: templateName}.Do(ctx, es.Client)
 
 	if err != nil {
 		es.log.Error(err, "error while deleting template", "templateName", templateName)
--- pkg/utils/elasticsearch_test.go
+++ pkg/utils/elasticsearch_test.go
@@ -15,6 +15,7 @@ limitations under the License.
 package utils
 
 import (
+	"context"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -43,8 +44,9 @@ func buildElasticsearch(t *testing.T) *Elasticsearch {
 }
 
 func deleteAll(elasticsearch *Elasticsearch) {
-	elasticsearch.DeleteIndex("k8s_epo_*")
-	elasticsearch.DeleteTemplate("k8s_epo_*")
+	ctx := context.Background()
+	elasticsearch.DeleteIndex(ctx, "k8s_epo_*")
+	elasticsearch.DeleteTemplate(ctx, "k8s_epo_*")
 }
 
 func TestEsConfig_String(t *testing.T) {
@@ -108,212 +110,222 @@ func TestElasticsearch_PingES(t *testing.T) {
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
-	err3 := elasticsearch.PingES()
+	err3 := elasticsearch.PingES(context.Background())
 	assert.Nil(err3)
 }
 
 func TestElasticsearch_CreateOrUpdateIndex(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_create_index"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"properties":{"description":{"type":"keyword"}}}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"properties":{"description":{"type":"keyword"}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
 	assert.Equal(StatusCreated, status.Status)
-	assert.True(*elasticsearch.existsIndex(indexName))
-	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	assert.True(*elasticsearch.existsIndex(ctx, indexName))
+	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(3), *replicas)
 	assert.Equal(int32(5), *shards)
-	properties, err := elasticsearch.getProperties(indexName)
+	properties, err := elasticsearch.getProperties(ctx, indexName)
 	assert.Nil(err)
 	assert.True(CompareJson(*properties, `{"properties" :{"description":{"type":"keyword"}}}`))
 }
 
 func TestElasticsearch_CreateOrUpdateIndex_WithType(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_create_index_with_type"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"_doc": {"properties":{"description":{"type":"keyword"}}}}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"_doc": {"properties":{"description":{"type":"keyword"}}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
 	assert.Equal(StatusCreated, status.Status)
-	assert.True(*elasticsearch.existsIndex(indexName))
+	assert.True(*elasticsearch.existsIndex(ctx, indexName))
 }
 
 func TestElasticsearch_CreateOrUpdateIndex_WithoutType(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_create_index_without_type"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"properties":{"description":{"type":"keyword"}}}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}, "mappings":{"properties":{"description":{"type":"keyword"}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
 	assert.Equal(StatusCreated, status.Status)
-	assert.True(*elasticsearch.existsIndex(indexName))
+	assert.True(*elasticsearch.existsIndex(ctx, indexName))
 }
 
 func TestElasticsearch_UpdateIndexMapping(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_update_settings"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"mappings":{"properties":{"description":{"type":"keyword"}}}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"mappings":{"properties":{"description":{"type":"keyword"}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
 	assert.Equal(StatusCreated, status.Status)
-	properties, err := elasticsearch.getProperties(indexName)
+	properties, err := elasticsearch.getProperties(ctx, indexName)
 	assert.Nil(err)
 	assert.True(CompareJson(*properties, `{"properties" :{"description":{"type":"keyword"}}}`))
 
 	//add new field in properties
-	status2, err := elasticsearch.updateIndexProperties(indexName, `{"mappings":{"properties":{"description":{"type":"keyword"}, "newField":{"type":"text"}}}}`)
+	status2, err := elasticsearch.updateIndexProperties(ctx, indexName, `{"mappings":{"properties":{"description":{"type":"keyword"}, "newField":{"type":"text"}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status2.HttpCodeStatus)
 	assert.Equal(StatusCreated, status2.Status)
-	properties2, err := elasticsearch.getProperties(indexName)
+	properties2, err := elasticsearch.getProperties(ctx, indexName)
 	assert.Nil(err)
 	assert.True(CompareJson(*properties2, `{"properties" :{"description":{"type":"keyword"}, "newField":{"type":"text"}}}`))
 
 	//change field type in properties => bad request
-	status3, err := elasticsearch.updateIndexProperties(indexName, `{"mappings":{"properties":{"description":{"type":"text"}, "newField":{"type":"text"}}}}`)
+	status3, err := elasticsearch.updateIndexProperties(ctx, indexName, `{"mappings":{"properties":{"description":{"type":"text"}, "newField":{"type":"text"}}}}`)
 	assert.NotNil(err)
 	assert.Equal("400", status3.HttpCodeStatus)
 	assert.Equal(StatusError, status3.Status)
-	properties3, err := elasticsearch.getProperties(indexName)
+	properties3, err := elasticsearch.getProperties(ctx, indexName)
 	assert.Nil(err)
 	assert.True(CompareJson(*properties3, `{"properties" :{"description":{"type":"keyword"}, "newField":{"type":"text"}}}`))
 
 	//update with less properties
-	status4, err := elasticsearch.updateIndexProperties(indexName, `{"mappings":{"properties":{"newField":{"type":"text"}}}}`)
+	status4, err := elasticsearch.updateIndexProperties(ctx, indexName, `{"mappings":{"properties":{"newField":{"type":"text"}}}}`)
 	assert.NotNil(err)
 	assert.Equal(StatusError, status4.Status)
-	properties4, err := elasticsearch.getProperties(indexName)
+	properties4, err := elasticsearch.getProperties(ctx, indexName)
 	assert.Nil(err)
 	assert.True(CompareJson(*properties4, `{"properties" :{"description":{"type":"keyword"}, "newField":{"type":"text"}}}`))
 }
 
 func TestElasticsearch_UpdateIndexSettings(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_update_settings"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
 	assert.Equal(StatusCreated, status.Status)
-	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(3), *replicas)
 	assert.Equal(int32(5), *shards)
 
 	//update replicas number
-	status2, err := elasticsearch.updateIndexSettings(indexName, `{"settings":{"number_of_replicas": "1", "number_of_shards": "5"}}`)
+	status2, err := elasticsearch.updateIndexSettings(ctx, indexName, `{"settings":{"number_of_replicas": "1", "number_of_shards": "5"}}`)
 	assert.Nil(err)
 	assert.Equal("200", status2.HttpCodeStatus)
 	assert.Equal(StatusCreated, status2.Status)
-	replicas2, shards2 := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	replicas2, shards2 := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(1), *replicas2)
 	assert.Equal(int32(5), *shards2)
 
 	//cannot update shards number
-	status3, err := elasticsearch.updateIndexSettings(indexName, `{"settings":{"number_of_replicas": "1", "number_of_shards": "7"}}`)
+	status3, err := elasticsearch.updateIndexSettings(ctx, indexName, `{"settings":{"number_of_replicas": "1", "number_of_shards": "7"}}`)
 	assert.NotNil(err)
 	assert.Equal(StatusError, status3.Status)
-	replicas3, shards3 := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	replicas3, shards3 := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(1), *replicas3)
 	assert.Equal(int32(5), *shards3)
 }
 
 func TestElasticsearch_UpdateIndexReplicas(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_update_index_setting"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3", "number_of_shards": "5"}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
-	assert.True(*elasticsearch.existsIndex(indexName))
-	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	assert.True(*elasticsearch.existsIndex(ctx, indexName))
+	replicas, shards := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(3), *replicas)
 	assert.Equal(int32(5), *shards)
 
-	status2, _, err2 := elasticsearch.updateIndexReplicas(indexName, 4)
+	status2, _, err2 := elasticsearch.updateIndexReplicas(ctx, indexName, 4)
 	assert.Nil(err2)
 	assert.Equal(200, status2)
-	replicas2, shards2 := elasticsearch.getNumberOfReplicasAndShards(indexName)
+	replicas2, shards2 := elasticsearch.getNumberOfReplicasAndShards(ctx, indexName)
 	assert.Equal(int32(4), *replicas2)
 	assert.Equal(int32(5), *shards2)
 }
 
 func TestElasticsearch_DeleteIndex(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	indexName := "k8s_epo_test_delete_index"
 
-	status, err := elasticsearch.CreateOrUpdateIndex(indexName, `{"settings":{"number_of_replicas": "3"}}`)
+	status, err := elasticsearch.CreateOrUpdateIndex(ctx, indexName, `{"settings":{"number_of_replicas": "3"}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
-	assert.True(*elasticsearch.existsIndex(indexName))
+	assert.True(*elasticsearch.existsIndex(ctx, indexName))
 
-	err2 := elasticsearch.DeleteIndex(indexName)
+	err2 := elasticsearch.DeleteIndex(ctx, indexName)
 	assert.Nil(err2)
-	assert.False(*elasticsearch.existsIndex(indexName))
+	assert.False(*elasticsearch.existsIndex(ctx, indexName))
 }
 
 func TestElasticsearch_CreateOrUpdateTemplate(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	templateName := "k8s_epo_test_create_template"
 
-	status, err := elasticsearch.CreateOrUpdateTemplate(templateName, `{"index_patterns": ["k8s_epo_test_*"], "mappings": {"properties": {}}}`)
+	status, err := elasticsearch.CreateOrUpdateTemplate(ctx, templateName, `{"index_patterns": ["k8s_epo_test_*"], "mappings": {"properties": {}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
-	assert.True(*elasticsearch.existsTemplate(templateName))
+	assert.True(*elasticsearch.existsTemplate(ctx, templateName))
 }
 
 func TestElasticsearch_CreateOrUpdateTemplate_WithType(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	templateName := "k8s_epo_test_create_template_with_type"
 
-	status, err := elasticsearch.CreateOrUpdateTemplate(templateName, `{"index_patterns": ["k8s_epo_test_*"], "mappings": {"my-type": {"properties": {}}}}`)
+	status, err := elasticsearch.CreateOrUpdateTemplate(ctx, templateName, `{"index_patterns": ["k8s_epo_test_*"], "mappings": {"my-type": {"properties": {}}}}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
-	assert.True(*elasticsearch.existsTemplate(templateName))
+	assert.True(*elasticsearch.existsTemplate(ctx, templateName))
 }
 
 func TestElasticsearch_DeleteTemplate(t *testing.T) {
+	ctx := context.Background()
 	assert := assert.New(t)
 	elasticsearch := buildElasticsearch(t)
 	defer deleteAll(elasticsearch)
 
 	templateName := "k8s_epo_test_delete_template"
 
-	status, err := elasticsearch.CreateOrUpdateTemplate(templateName, `{"index_patterns": ["k8s_epo_test_*"]}`)
+	status, err := elasticsearch.CreateOrUpdateTemplate(ctx, templateName, `{"index_patterns": ["k8s_epo_test_*"]}`)
 	assert.Nil(err)
 	assert.Equal("200", status.HttpCodeStatus)
-	assert.True(*elasticsearch.existsTemplate(templateName))
+	assert.True(*elasticsearch.existsTemplate(ctx, templateName))
 
-	err2 := elasticsearch.DeleteTemplate(templateName)
+	err2 := elasticsearch.DeleteTemplate(ctx, templateName)
 	assert.Nil(err2)
-	assert.False(*elasticsearch.existsTemplate(templateName))
+	assert.False(*elasticsearch.existsTemplate(ctx, templateName))
 }
